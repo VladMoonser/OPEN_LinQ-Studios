ef893b5564f4b262121a0115c9dd1006f7c99497
Vlado <94527377+VladMoonser@users.noreply.github.com>
2023-03-02 17:32:11 +0100

    List of songs released by Monstercat on Roblox.

---
+++ monstercat.lua
@@ -0,0 +1,527 @@
+return {
+	[5409360995] = {
+		ImageId = 137654766,
+		AudioId = 5409360995,
+		Name = "Shiawase",
+		Author = "Dion Timmer",
+		Releaser = "Monstercat"
+	};
+	[5410085763] = {
+		ImageId = 137654766,
+		AudioId = 5410085763,
+		Name = "PLAY",
+		Author = "Tokyo Machine",
+		Releaser = "Monstercat"
+	};
+	[7028518546] = {
+		ImageId = 137654766,
+		AudioId = 7028518546,
+		Name = "New Horizons",
+		Author = "Protostar",
+		Releaser = "Monstercat"
+	};
+	[7023635858] = {
+		ImageId = 137654766,
+		AudioId = 7023635858,
+		Name = "Vex",
+		Author = "Bensley",
+		Releaser = "Monstercat"
+	};
+	[7029024726] = {
+		ImageId = 137654766,
+		AudioId = 7029024726,
+		Name = "Bloom",
+		Author = "Throttle",
+		Releaser = "Monstercat"
+	};
+	[7024220835] = {
+		ImageId = 137654766,
+		AudioId = 7024220835,
+		Name = "Easter Egg",
+		Author = "Nitro Fun",
+		Releaser = "Monstercat"
+	};
+	[7024233823] = {
+		ImageId = 137654766,
+		AudioId = 7024233823,
+		Name = "Safe & Sound",
+		Author = "Nitro Fun, Danyka Nadeau",
+		Releaser = "Monstercat"
+	};
+	[7028913008] = {
+		ImageId = 137654766,
+		AudioId = 7028913008,
+		Name = "Flow",
+		Author = "Slippy",
+		Releaser = "Monstercat"
+	};
+	[7024035759] = {
+		ImageId = 137654766,
+		AudioId = 7024035759,
+		Name = "Get to Know You",
+		Author = "Dexter King, Aviella",
+		Releaser = "Monstercat"
+	};
+	[7029017448] = {
+		ImageId = 137654766,
+		AudioId = 7029017448,
+		Name = "Play It Cool",
+		Author = "Terry Zhong, Conro",
+		Releaser = "Monstercat"
+	};
+	[7023828725] = {
+		ImageId = 137654766,
+		AudioId = 7023828725,
+		Name = "Color",
+		Author = "Grant",
+		Releaser = "Monstercat"
+	};
+	[7028856935] = {
+		ImageId = 137654766,
+		AudioId = 7028856935,
+		Name = "Glow",
+		Author = "Shingo Nakamura",
+		Releaser = "Monstercat"
+	};
+	[7023771708] = {
+		ImageId = 137654766,
+		AudioId = 7023771708,
+		Name = "Still",
+		Author = "Glacier",
+		Releaser = "Monstercat"
+	};
+	[7023670701] = {
+		ImageId = 137654766,
+		AudioId = 7023670701,
+		Name = "Wish",
+		Author = "CloudNone",
+		Releaser = "Monstercat"
+	};
+	[7023680426] = {
+		ImageId = 137654766,
+		AudioId = 7023680426,
+		Name = "All I Want",
+		Author = "Conro",
+		Releaser = "Monstercat"
+	};
+	[7028932563] = {
+		ImageId = 137654766,
+		AudioId = 7028932563,
+		Name = "Hours",
+		Author = "Soupandreas & Inverness",
+		Releaser = "Monstercat"
+	};
+	[7023887630] = {
+		ImageId = 137654766,
+		AudioId = 7023887630,
+		Name = "Expedition",
+		Author = "Hyper Potions, Nokae",
+		Releaser = "Monstercat"
+	};
+	[7028985831] = {
+		ImageId = 137654766,
+		AudioId = 7028985831,
+		Name = "Fire",
+		Author = "Stonebank",
+		Releaser = "Monstercat"
+	};
+	[7024028859] = {
+		ImageId = 137654766,
+		AudioId = 7024028859,
+		Name = "Only You",
+		Author = "Dexter King, Alexis Donn",
+		Releaser = "Monstercat"
+	};
+	[7028907200] = {
+		ImageId = 137654766,
+		AudioId = 7028907200,
+		Name = "Uncharted Waters",
+		Author = "Shingo Nakamura",
+		Releaser = "Monstercat"
+	};
+	[7029051434] = {
+		ImageId = 137654766,
+		AudioId = 7029051434,
+		Name = "Good 4 Me",
+		Author = "Vindata",
+		Releaser = "Monstercat"
+	};
+	[7024332460] = {
+		ImageId = 137654766,
+		AudioId = 7024332460,
+		Name = "Shaku",
+		Author = "Pegboard Nerds",
+		Releaser = "Monstercat"
+	};
+	[7029092469] = {
+		ImageId = 137654766,
+		AudioId = 7029092469,
+		Name = "Love Is",
+		Author = "Vintage & Morelli, ...",
+		Releaser = "Monstercat"
+	};
+	[7024254685] = {
+		ImageId = 137654766,
+		AudioId = 7024254685,
+		Name = "From Dust & Ashes",
+		Author = "Notaker",
+		Releaser = "Monstercat"
+	};
+	[7024183256] = {
+		ImageId = 137654766,
+		AudioId = 7024183256,
+		Name = "Balance",
+		Author = "Martin Graff",
+		Releaser = "Monstercat"
+	};
+	[7029031068] = {
+		ImageId = 137654766,
+		AudioId = 7029031068,
+		Name = "Language of Angels",
+		Author = "Varien",
+		Releaser = "Monstercat"
+	};
+	[7023407320] = {
+		ImageId = 137654766,
+		AudioId = 7023407320,
+		Name = "Sleep",
+		Author = "A.M.R, Terry Da Libra",
+		Releaser = "Monstercat"
+	};
+	[7028977687] = {
+		ImageId = 137654766,
+		AudioId = 7028977687,
+		Name = "Whatcha Waiting For",
+		Author = "Stonebank",
+		Releaser = "Monstercat"
+	};
+	[7028919492] = {
+		ImageId = 137654766,
+		AudioId = 7028919492,
+		Name = "Found A Reason",
+		Author = "SMLE",
+		Releaser = "Monstercat"
+	};
+	[7023704173] = {
+		ImageId = 137654766,
+		AudioId = 7023704173,
+		Name = "Imprints",
+		Author = "djimboh",
+		Releaser = "Monstercat"
+	};
+	[7023617400] = {
+		ImageId = 137654766,
+		AudioId = 7023617400,
+		Name = "Destroy Me",
+		Author = "Bad Computer",
+		Releaser = "Monstercat"
+	};
+	[7028997537] = {
+		ImageId = 137654766,
+		AudioId = 7028997537,
+		Name = "What It's Like",
+		Author = "Subtact, Noctilucent",
+		Releaser = "Monstercat"
+	};
+	[7024101188] = {
+		ImageId = 137654766,
+		AudioId = 7024101188,
+		Name = "Same Mistake",
+		Author = "F. Derelle, J. Rubin",
+		Releaser = "Monstercat"
+	};
+	[7028877251] = {
+		ImageId = 137654766,
+		AudioId = 7028877251,
+		Name = "Sapporo",
+		Author = "Shingo Nakamura",
+		Releaser = "Monstercat"
+	};
+	[7023733671] = {
+		ImageId = 137654766,
+		AudioId = 7023733671,
+		Name = "Constellations",
+		Author = "Elypsis",
+		Releaser = "Monstercat"
+	};
+	[7029070008] = {
+		ImageId = 137654766,
+		AudioId = 7029070008,
+		Name = "Ascension",
+		Author = "Vintage & Morelli",
+		Releaser = "Monstercat"
+	};
+	[7028527348] = {
+		ImageId = 137654766,
+		AudioId = 7028527348,
+		Name = "Where I Belong",
+		Author = "Protostar, Emma McGann",
+		Releaser = "Monstercat"
+	};
+	[7028557220] = {
+		ImageId = 137654766,
+		AudioId = 7028557220,
+		Name = "Motion",
+		Author = "Rogue",
+		Releaser = "Monstercat"
+	};
+	[7023598688] = {
+		ImageId = 137654766,
+		AudioId = 7023598688,
+		Name = "Clarity",
+		Author = "Bad Computer",
+		Releaser = "Monstercat"
+	};
+	[7024132063] = {
+		ImageId = 137654766,
+		AudioId = 7024132063,
+		Name = "Your Pain",
+		Author = "Koven",
+		Releaser = "Monstercat"
+	};
+	[7023760529] = {
+		ImageId = 137654766,
+		AudioId = 7023760529,
+		Name = "Enough",
+		Author = "Glacier",
+		Releaser = "Monstercat"
+	};
+	[7023370016] = {
+		ImageId = 137654766,
+		AudioId = 7023370016,
+		Name = "Beginnings",
+		Author = "A.M.R",
+		Releaser = "Monstercat"
+	};
+	[7024245182] = {
+		ImageId = 137654766,
+		AudioId = 7024245182,
+		Name = "Endless Love",
+		Author = "Nōpi",
+		Releaser = "Monstercat"
+	};
+	[7028548115] = {
+		ImageId = 137654766,
+		AudioId = 7028548115,
+		Name = "Move Me",
+		Author = "Rogue",
+		Releaser = "Monstercat"
+	};
+	[7024121782] = {
+		ImageId = 137654766,
+		AudioId = 7024121782,
+		Name = "Timeless",
+		Author = "Jay FM",
+		Releaser = "Monstercat"
+	};
+	[7029099738] = {
+		ImageId = 137654766,
+		AudioId = 7029099738,
+		Name = "Wishful Thinking",
+		Author = "WRLD, Keepsake",
+		Releaser = "Monstercat"
+	};
+	[7028970358] = {
+		ImageId = 137654766,
+		AudioId = 7028970358,
+		Name = "JC-08",
+		Author = "Stephen Walking",
+		Releaser = "Monstercat"
+	};
+	[7023445033] = {
+		ImageId = 137654766,
+		AudioId = 7023445033,
+		Name = "About U",
+		Author = "Aiobahn, Vin",
+		Releaser = "Monstercat"
+	};
+	[7023435987] = {
+		ImageId = 137654766,
+		AudioId = 7023435987,
+		Name = "Portrait Of You",
+		Author = "A.M.R",
+		Releaser = "Monstercat"
+	};
+	[7023720291] = {
+		ImageId = 137654766,
+		AudioId = 7023720291,
+		Name = "You Used To",
+		Author = "Duumu, I See Colors, Luma",
+		Releaser = "Monstercat"
+	};
+	[7024280102] = {
+		ImageId = 137654766,
+		AudioId = 7024280102,
+		Name = "For You",
+		Author = "Odsen",
+		Releaser = "Monstercat"
+	};
+	[7024165463] = {
+		ImageId = 137654766,
+		AudioId = 7024165463,
+		Name = "Ten Years Ago Today",
+		Author = "Mango",
+		Releaser = "Monstercat"
+	};
+	[7029041793] = {
+		ImageId = 137654766,
+		AudioId = 7029041793,
+		Name = "Let Me Down",
+		Author = "Veeshy, B. Mignacca",
+		Releaser = "Monstercat"
+	};
+	[7023741506] = {
+		ImageId = 137654766,
+		AudioId = 7023741506,
+		Name = "I Miss You",
+		Author = "Elypsis",
+		Releaser = "Monstercat"
+	};
+	[7023846047] = {
+		ImageId = 137654766,
+		AudioId = 7023846047,
+		Name = "Can You Hear It",
+		Author = "Grant Bowtie",
+		Releaser = "Monstercat"
+	};
+	[7024154355] = {
+		ImageId = 137654766,
+		AudioId = 7024154355,
+		Name = "Feels Like Love",
+		Author = "Laszlo",
+		Releaser = "Monstercat"
+	};
+	[7023861220] = {
+		ImageId = 137654766,
+		AudioId = 7023861220,
+		Name = "Video Games",
+		Author = "Half an Orange",
+		Releaser = "Monstercat"
+	};
+	[7028570258] = {
+		ImageId = 137654766,
+		AudioId = 7028570258,
+		Name = "Skin",
+		Author = "Rome in Silver, ...",
+		Releaser = "Monstercat"
+	};
+	[7023800698] = {
+		ImageId = 137654766,
+		AudioId = 7023800698,
+		Name = "Over It",
+		Author = "Glaue",
+		Releaser = "Monstercat"
+	};
+	[7023650590] = {
+		ImageId = 137654766,
+		AudioId = 7023650590,
+		Name = "Descender",
+		Author = "Case & Point",
+		Releaser = "Monstercat"
+	};
+	[7029005367] = {
+		ImageId = 137654766,
+		AudioId = 7029005367,
+		Name = "Hold On",
+		Author = "Summer Was Fun, Q'AILA",
+		Releaser = "Monstercat"
+	};
+	[5410080857] = {
+		ImageId = 137654766,
+		AudioId = 5410080857,
+		Name = "Neon",
+		Author = "F.O.O.L, Midranger",
+		Releaser = "Monstercat"
+	};
+	[7024109415] = {
+		ImageId = 137654766,
+		AudioId = 7024109415,
+		Name = "Insight",
+		Author = "Jay FM",
+		Releaser = "Monstercat"
+	};
+	[7023459476] = {
+		ImageId = 137654766,
+		AudioId = 7023459476,
+		Name = "Closer",
+		Author = "Anthony Nikita",
+		Releaser = "Monstercat"
+	};
+	[7023785633] = {
+		ImageId = 137654766,
+		AudioId = 7023785633,
+		Name = "The Way We Do",
+		Author = "Glaue",
+		Releaser = "Monstercat"
+	};
+	[7024143472] = {
+		ImageId = 137654766,
+		AudioId = 7024143472,
+		Name = "All for Nothing",
+		Author = "Koven",
+		Releaser = "Monstercat"
+	};
+	[5410083549] = {
+		ImageId = 137654766,
+		AudioId = 5410083549,
+		Name = "Let's Talk",
+		Author = "Rogue",
+		Releaser = "Monstercat"
+	};
+	[7028540590] = {
+		ImageId = 137654766,
+		AudioId = 7028540590,
+		Name = "Thinking About You",
+		Author = "Rameses B",
+		Releaser = "Monstercat"
+	};
+	[7023690024] = {
+		ImageId = 137654766,
+		AudioId = 7023690024,
+		Name = "Labyrinth",
+		Author = "Direct, Essenger",
+		Releaser = "Monstercat"
+	};
+	[7023749823] = {
+		ImageId = 137654766,
+		AudioId = 7023749823,
+		Name = "Mimi",
+		Author = "Eskai",
+		Releaser = "Monstercat"
+	};
+	[7024324344] = {
+		ImageId = 137654766,
+		AudioId = 7024324344,
+		Name = "In My Bones",
+		Author = "Odsen",
+		Releaser = "Monstercat"
+	};
+	[7024349679] = {
+		ImageId = 137654766,
+		AudioId = 7024349679,
+		Name = "Joy Is Pain",
+		Author = "PROFF",
+		Releaser = "Monstercat"
+	};
+	[7029083554] = {
+		ImageId = 137654766,
+		AudioId = 7029083554,
+		Name = "Dreamers",
+		Author = "Vintage & Morelli, ...",
+		Releaser = "Monstercat"
+	};
+	[7024357019] = {
+		ImageId = 137654766,
+		AudioId = 7024357019,
+		Name = "For The Last Time",
+		Author = "PROFF",
+		Releaser = "Monstercat"
+	};
+	[7029061992] = {
+		ImageId = 137654766,
+		AudioId = 7029061992,
+		Name = "Union",
+		Author = "Vindata",
+		Releaser = "Monstercat"
+	}
+}
